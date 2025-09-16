8f08945f930559fc02738ce369abf70d7ef2c9bc
uniuni7 <uniuni0141@docomo.ne.jp>
2025-09-17 05:14:17 +0900

    今日の歩数をDBに渡すAPIの設計＋ミドルウェアの導入

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "No token" });
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.userId = decoded.id;
+    next();
+  } catch {
+    res.status(401).json({ error: "Invalid token" });
+  }
+}
+
+module.exports = authMiddleware;
