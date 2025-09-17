7e858754fbc09e0e667e65276c73f65a56a016b2
uniuni7 <uniuni0141@docomo.ne.jp>
2025-09-18 02:58:21 +0900

    ユーザーの勝敗数を更新するAPI

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const testRoute = require("./routers/test");
 const authRoute = require("./routers/auth");
 const stepsRoute = require("./routers/steps");
 const userRoute = require("./routers/user");
@@ -13,7 +12,6 @@ const PORT = 5000;
 //corsはフロントのlocalhostとlocalhost:5000間のアクセスを許可する(クロスオリジンポリシー)
 app.use(cors());
 app.use(express.json());
-app.use("/api/test", testRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/steps", stepsRoute);
 app.use("/api/user", userRoute);
