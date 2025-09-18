c72c1c1496b7b21cbf08ebdbb78b70f34994749c
uniuni7 <uniuni0141@docomo.ne.jp>
2025-09-19 02:36:14 +0900

    バトルのログを記録するAPIの実装

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const authRoute = require("./routers/auth");
 const stepsRoute = require("./routers/steps");
-const userRoute = require("./routers/user");
+const battleRoute = require("./routers/battle");
 const rankingRoute = require("./routers/ranking");
 
 require("dotenv").config();
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/steps", stepsRoute);
-app.use("/api/user", userRoute);
+app.use("/api/battle", battleRoute);
 app.use("/api/ranking", rankingRoute);
 
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
